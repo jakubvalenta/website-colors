bc565cabf1495464d1343231f5b3b506e6cc4f7f
Jakub Valenta <jakub@jakubvalenta.cz>
2020-03-30 14:23:00 +0200

    Create Datawrapper chart

--- web_colors/cli.py
+++ web_colors/cli.py
@@ -4,7 +4,7 @@ from typing import IO
 
 import click
 
-from web_colors.web_colors import analyze_image
+from web_colors.web_colors import analyze_image, create_chart
 
 logger = logging.getLogger(__name__)
 
@@ -12,13 +12,23 @@ logger = logging.getLogger(__name__)
 @click.command()
 @click.argument('input_image', type=click.File('rb'))
 @click.argument('output_csv', type=click.File('wt'))
+@click.argument('base_url', type=str, default='http://api.datawrapper.local')
+@click.argument('auth_token', envvar='AUTH_TOKEN', type=str)
 @click.option(
     '--verbose', '-v', is_flag=True, help='Enable debugging output',
 )
-def main(input_image: IO, output_csv: IO, verbose: bool):
+def main(
+    input_image: IO,
+    output_csv: IO,
+    base_url: str,
+    auth_token: str,
+    verbose: bool,
+):
     """Analyze `input_image` and print results to `output_csv`."""
     if verbose:
         logging.basicConfig(
             stream=sys.stdout, level=logging.INFO, format='%(message)s'
         )
-    analyze_image(input_image, output_csv)
+    data = analyze_image(input_image)
+    data.to_csv(output_csv, index_label='color')
+    create_chart(base_url, auth_token, title='Colors', data=data)
--- web_colors/web_colors.py
+++ web_colors/web_colors.py
@@ -1,6 +1,8 @@
+import json
 from typing import IO, Tuple
 
 import pandas as pd
+import requests
 from PIL import Image
 
 RGB = Tuple[int, int, int]
@@ -11,18 +13,36 @@ def rgb2hex(rgb: RGB) -> str:
     return f'#{r:02x}{g:02x}{b:02x}'
 
 
-def analyze_image(f_in: IO, f_out: IO, bg_color: RGB = (255, 255, 255)):
+def analyze_image(f_in: IO, bg_color: RGB = (255, 255, 255)) -> pd.Series:
     im = Image.open(f_in)
     pixels = pd.Series(list(im.getdata()), name='frequency')
     fg_pixels = pixels[pixels != bg_color].apply(rgb2hex)
     fg_counts = fg_pixels.value_counts(normalize=True)
     fg_counts_large = fg_counts[fg_counts >= 0.01]
-    fg_counts_large.to_csv(f_out, index_label='color')
+    return fg_counts_large
 
 
-def create_chart(data: pd.Series):
+def create_chart(base_url: str, auth_token: str, title: str, data: pd.Series):
     props = {
-        'visualize': {
-            'custom_colors': [[color, color] for color in data.index]
-        }
+        'title': title,
+        'type': 'd3-pies',
+        'metadata': {
+            'visualize': {
+                'custom-colors': {color: color for color in data.index},
+                'group': {'num_slices': 50},
+            },
+        },
     }
+    headers = {'Authorization': f'Bearer {auth_token}'}
+    res = requests.post(
+        base_url + '/v3/charts', headers=headers, data=json.dumps(props)
+    )
+    res.raise_for_status()
+    chart_id = res.json()['id']
+    csv = data.to_csv(index_label='color')
+    res = requests.put(
+        base_url + f'/charts/{chart_id}/data',
+        headers={**headers, 'Content-Type': 'text/csv'},
+        data=csv,
+    )
+    res.raise_for_status()
